25a4526bd4486536ac6ddc1a14e877a306cf74a0
Fatih Ar <arfatiih@gmail.com>
2020-12-28 02:45:13 +0300

    universiteler sayfasinda databaseden bilgi cekimi farklilasti

--- lib/universiteler.dart
+++ lib/universiteler.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -15,23 +15,62 @@ class _UniversitelerState extends State<Universiteler> {
       appBar: AppBar(
         title: Text("Universiteler"),
       ),
-      body: StreamBuilder(
+      body: UserInformation(),
+
+
+    /*StreamBuilder(
         stream:
-            FirebaseFirestore.instance.collection("universiteler").snapshots(),
+        FirebaseFirestore.instance.collection("universiteler").snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) return Text("Loading Data.. Please wait..");
-          return ListView.builder(itemCount:snapshot.data.documents.length,itemBuilder: (context, int index) {
-            return ListTile(
-              leading: Icon(Icons.school),
-              title: Text('${snapshot.data.documents[index]['isim']}\n '
-                  'Minimum Toefl Puanı: ${snapshot.data.documents[index]['min_toefl']} \n'
-                  'Minimum GPA Puanı: ${snapshot.data.documents[index]['min_gpa']} \n'
-                  'Şehir: ${snapshot.data.documents[index]['sehir']}\n'),
-              subtitle: Text('${snapshot.data.documents[index]['bolumler']}'),
-            );
-          });
+          return ListView.builder(itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, int index) {
+                return ListTile(
+                  shape: RoundedRectangleBorder(),
+                  leading: Icon(Icons.school),
+                  title: Text('${snapshot.data.documents[index]['isim']}\n '
+                      'Minimum Toefl Puanı: ${snapshot.data
+                      .documents[index]['min_toefl']} \n'
+                      'Minimum GPA Puanı: ${snapshot.data
+                      .documents[index]['min_gpa']} \n'
+                      'Şehir: ${snapshot.data.documents[index]['sehir']}\n'),
+                  subtitle: Text(
+                      '${snapshot.data.documents[index]['bolumler']}'),
+                );
+              });
         },
-      ),
+      ),*/
+    );
+  }
+}
+
+class UserInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference universiteler = FirebaseFirestore.instance.collection('universiteler');
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: universiteler.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return new ListView(
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            return new ListTile(
+              title: new Text(document.data()['sehir'] + "  " + document.data()['isim']),
+              subtitle: new Text('${document.data()['min_toefl']}' + " - " + "${document.data()['min_gpa']}" + "\n" +
+                  document.data()['bolumler'][0] + "\n" +
+                  document.data()['bolumler'][1]),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
